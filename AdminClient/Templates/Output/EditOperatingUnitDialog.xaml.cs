d862c394ce3341832152534c68f33ac46e4d720e
Naveen Thakur <naveen@machinemedicine.com>
2024-12-29 13:00:40 +0000

    Templates might come in useful one day

---
+++ AdminClient/Templates/Output/EditOperatingUnitDialog.xaml.cs
@@ -0,0 +1,17 @@
+using System.Windows.Controls;
+
+namespace AdminClient.Views
+{
+    /// <summary>
+    /// Interaction logic for EditOperatingUnitDialog.xaml
+    /// </summary>
+    public partial class EditOperatingUnitDialog : UserControl
+    {
+        public EditOperatingUnitDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+

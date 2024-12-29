91305e897ab4d407facf14f6b14343fbaa48aac8
Naveen Thakur <naveen@machinemedicine.com>
2024-12-29 11:34:47 +0000

    Start creating other collections. Also experiment with T4.

---
+++ AdminClient/Templates/Output/CreateOperatingUnitDialog.xaml.cs
@@ -0,0 +1,17 @@
+using System.Windows.Controls;
+
+namespace AdminClient.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateOperatingUnitDialog.xaml
+    /// </summary>
+    public partial class CreateOperatingUnitDialog : UserControl
+    {
+        public CreateOperatingUnitDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
